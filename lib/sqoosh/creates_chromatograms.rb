5bdb562448093a121292801970045a5590e87f7e
John Prince <jtprince@gmail.com>
2014-05-01 17:29:48 -0600

    adding verbose option

--- lib/sqoosh/creates_chromatograms.rb
+++ lib/sqoosh/creates_chromatograms.rb
@@ -29,10 +29,18 @@ module Sqoosh
           end
           start ||= found_start
           stop ||= found_stop
+          puts "Found start m/z: #{start} and stop m/z: #{stop}" if $VERBOSE
         end
 
+        print "binning spectra [100 per .]:" if $VERBOSE
+        cnt = 0
         mzml.each do |spectrum|
           next unless spectrum.ms_level == 1
+          cnt += 1
+          if $VERBOSE && cnt % 100 == 0
+            print "."
+            $stdout.flush
+          end
           retention_times << spectrum.retention_time
           reply = Binneroc.bin(
             *spectrum.mzs_and_intensities, 
@@ -53,6 +61,7 @@ module Sqoosh
         end
       end
 
+      puts "transposing into chromatograms" if $VERBOSE
       binned_spectra.transpose.zip(mz_values_array).map do |intensities, mz|
         Sqoosh::Chromatogram.new retention_times, intensities, Range.new(mz, mz + increment, exclude_end)
       end
